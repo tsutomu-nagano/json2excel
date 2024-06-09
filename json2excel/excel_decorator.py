f88e24459d7b090dd8959beb9450b09ad40b5246
tsutomu-nagano <copyxshow@gmail.com>
2024-06-09 22:48:18 +0900

    罫線とセルの塗りつぶしの機能追加

--- json2excel/excel_decorator.py
+++ json2excel/excel_decorator.py
@@ -10,6 +10,27 @@ import utils
 
 class ExcelDecorator():
 
+    @staticmethod
+    def add_border(cell, position, side):
+        if position == "all":
+            border_ = {"left": side ,"right": side, "top": side, "bottom": side}
+        else:
+            border_ = {"left": cell.border.left ,"right": cell.border.right, "top": cell.border.top, "bottom": cell.border.bottom}
+            border_[position] = side
+        
+        cell.border = openpyxl.styles.Border(** border_)
+
+    def get_sheet_names(wb, info):
+
+        if "re" in info:
+            name_match = lambda s : re.match(info["re"], s)
+
+        if "text" in info:
+            name_match = lambda s : info["text"] == s
+
+        return([s for s in wb.sheetnames if name_match(s)])
+
+
     @classmethod
     def decoration(cls, config, dest):
         
@@ -19,33 +40,83 @@ class ExcelDecorator():
 
         wb = openpyxl.load_workbook(dest)
 
-        for hyperlink in deco["hyperlink"]:
+        if "hyperlink" in deco:
+            for hyperlink in deco["hyperlink"]:
 
-            from_sheet_info = hyperlink["from"]["sheet"]
-            from_address = hyperlink["from"]["address"]
-            to_sheet_info = hyperlink["to"]["sheet"]
-            to_address = hyperlink["to"]["address"]
+                from_sheet_info = hyperlink["from"]["sheet"]
+                from_address = hyperlink["from"]["address"]
+                to_sheet_info = hyperlink["to"]["sheet"]
+                to_address = hyperlink["to"]["address"]
 
-            for r in utils.a1_to_range(from_address):
+                for r in utils.a1_to_range(from_address):
 
-                cell = wb[from_sheet_info["text"]][r["address"]]
+                    cell = wb[from_sheet_info["text"]][r["address"]]
 
-                if to_sheet_info["text"] == "$.":
-                    to_sheet = cell.value
+                    if to_sheet_info["text"] == "$.":
+                        to_sheet = cell.value
 
 
-                if to_sheet in wb.sheetnames:
+                    if to_sheet in wb.sheetnames:
 
-                    cell.hyperlink = f"#{to_sheet}!{to_address}"
-                    cell.font = openpyxl.styles.Font(color="0000FF", underline="single")
+                        cell.hyperlink = f"#{to_sheet}!{to_address}"
+                        cell.font = openpyxl.styles.Font(color="0000FF", underline="single")
 
-                    if "direction" in hyperlink:
-                        direction = hyperlink["direction"]
-                        if direction == "both":
-                            
-                            cell = wb[to_sheet][to_address]
-                            cell.hyperlink = f"#{from_sheet_info['text']}!{r['address']}"
-                            cell.font = openpyxl.styles.Font(color="0000FF", underline="single")
+                        if "direction" in hyperlink:
+                            direction = hyperlink["direction"]
+                            if direction == "both":
+                                
+                                cell = wb[to_sheet][to_address]
+                                cell.hyperlink = f"#{from_sheet_info['text']}!{r['address']}"
+                                cell.font = openpyxl.styles.Font(color="0000FF", underline="single")
+
+        if "border" in deco:
+            for border in deco["border"]:
+                
+                sheet_names = ExcelDecorator.get_sheet_names(wb, border["sheet"])
+                draws = border["draw"]
+
+                for sheet_name in sheet_names:
+
+                    ranges = utils.a1_to_range(utils.convert_auto_range(border["address"], dest, sheet_name))
+
+                    for draw in draws:
+                        draw_ = draw.split("-")
+                        target = draw_[0]
+                        style = draw_[1]
+
+                        side_ = openpyxl.styles.Side(border_style = style, color='000000')
+
+                        if target == "grid":
+                            [ExcelDecorator.add_border(wb[sheet_name][r["address"]], "all", side_) for r in ranges]
+
+                        if target == "around":
+                            arounds = utils.around_ranges(ranges)
+
+                            [ExcelDecorator.add_border(wb[sheet_name][r["address"]], position, side_) for position, ranges in arounds.items() for r in ranges]
+
+                        if target in ["bottom", "top", "left", "right"]:
+                            [ExcelDecorator.add_border(wb[sheet_name][r["address"]], target, side_) for r in ranges]
+
+        if "fill" in deco:
+
+            for fill in deco["fill"]:
+                sheet_names = ExcelDecorator.get_sheet_names(wb, fill["sheet"])
+
+                for sheet_name in sheet_names:
+
+                    ranges = utils.a1_to_range(utils.convert_auto_range(fill["address"], dest, sheet_name))
+
+                    for color in fill["color"]:
+                        fill_ = openpyxl.styles.fills.PatternFill(patternType='solid', fgColor=color["value"])
+
+                        text_match = lambda t : True
+                        if "text" in color:
+                            if "match" in color["text"]:
+                                text_match = lambda t : t in color["text"]["match"]
+
+                        for r in [r for r in ranges if text_match(wb[sheet_name][r["address"]].value)]:
+                            wb[sheet_name][r["address"]].fill = fill_
+
 
         wb.save(dest)
         wb.close()
--- json2excel/utils.py
+++ json2excel/utils.py
@@ -1,7 +1,53 @@
 
 import openpyxl
+import re
+import pandas as pd
 from itertools import product
 
+
+def around_ranges(ranges):
+    rows = [r["row"] for r in ranges]
+    cols = [r["col"] for r in ranges]
+
+    row_min = min(rows)
+    col_min = min(cols)
+    row_max = max(rows)
+    col_max = max(cols)
+
+    ret = {}
+    ret["left"] = [r for r in ranges if r["col"] == col_min]
+    ret["right"] = [r for r in ranges if r["col"] == col_max]
+    ret["top"] = [r for r in ranges if r["row"] == row_min]
+    ret["bottom"] = [r for r in ranges if r["row"] == row_max]
+
+    return(ret)
+
+
+def convert_auto_range(a1, src, sheet_name):
+
+    ptn = "(?P<FROM>.+):\$auto(\-(?P<DIRECTION>.+))?"
+    addr = re.match(ptn, a1)
+
+    if addr:
+        addr_from = addr["FROM"]
+        addr_direction = addr["DIRECTION"]
+
+        col_str, row = openpyxl.utils.cell.coordinate_from_string(addr_from)
+        col = openpyxl.utils.cell.column_index_from_string(col_str)
+
+        df = pd.read_excel(src, sheet_name = sheet_name, skiprows = row - 1, header = None).dropna(axis=1, how='all')
+
+        col_add = 1 if addr_direction == "row" else len(df.columns.tolist()) 
+        row_add = 1 if addr_direction == "col" else len(df)
+
+        addr_to = f"{openpyxl.utils.get_column_letter(col + col_add - 1)}{row + row_add - 1}"
+
+        ret = f"{addr_from}:{addr_to}"
+
+    else:
+        ret = a1
+
+    return(ret)
 
 
 def a1_to_range_core(a1):
